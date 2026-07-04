aeef5d3d40dc74b2bf28dbc5f6f732cd7e922a18
kunalraj0212 <razzkunal7@gmail.com>
2026-07-04 16:16:41 +0530

    feat(dsa): add if_else logic in conditional statements

---
+++ day 03 Conditional statements/if_else.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout << "Enetr your age : ";
+    cin >> age;
+
+    if(-5) {
+        cout << "can vote" << endl;
+    }
+
+    if(age >= 45) {
+        cout << "contest for elections" << endl;
+    }
+
+    return 0;
+}
